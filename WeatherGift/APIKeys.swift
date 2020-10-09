c077b92626e1413b2456da04eac529ba25208d45
Lazaro Alvelaez <lazaroalvelaez@bc.edu>
2020-10-09 17:24:59 -0400

    finished 6.4

---
+++ WeatherGift/APIKeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIKeys.swift
+//  WeatherGift
+//
+//  Created by Lazaro Alvelaez on 10/9/20.
+//
+
+import Foundation
+
+struct APIKeys {
+    static let googlePlacesKey = "AIzaSyCFkh5-jpQmKfl7wy6OnVoVo_Mx61TjQpw"
+}
